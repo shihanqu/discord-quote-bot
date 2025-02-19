01010d18f600f024e05efd3b11793fb53057df4b
shihanqu <shihanqu@gmail.com>
2025-02-19 09:48:43 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 BOT_TOKEN = "[YOUR BOT TOKEN HERE]"  # Replace with your actual bot token!
 DATABASE_FILE = "quotes.db"
-REACTION_EMOJI = 12345123451234512345 # ASCII EMOJI IN QUOTES, OR CUSTOM EMOJI ID
+REACTION_EMOJI = 12345123451234512345 # ASCII EMOJI IN QUOTES "👍", OR CUSTOM EMOJI ID
 ADMIN_ROLE_NAME = "SuperAdmin"  # Replace with your desired admin role name
 WEEKLY_QUOTE_CHANNEL_ID = 134563456345634563456345 # Replace with your channel ID for weekly quote.
 # Add a toggle for the author repeat
