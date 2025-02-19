8ce5a089efed92979fe7e667781571ce700bf8b5
shihanqu <shihanqu@gmail.com>
2025-02-19 09:44:17 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+BOT_TOKEN = "[YOUR BOT TOKEN HERE]"  # Replace with your actual bot token!
+DATABASE_FILE = "quotes.db"
+REACTION_EMOJI = 12345123451234512345 # ASCII EMOJI IN QUOTES, OR CUSTOM EMOJI ID
+ADMIN_ROLE_NAME = "SuperAdmin"  # Replace with your desired admin role name
+WEEKLY_QUOTE_CHANNEL_ID = 134563456345634563456345 # Replace with your channel ID for weekly quote.
+# Add a toggle for the author repeat
+AUTHOR_REPEAT_PREVENTION = True #Set to False to allow same author to appear multiple times.
